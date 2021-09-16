e9ccd8e4a2132e0949fa50d8151584efdb65a87e
aditipolkam <aditipolkam@gmail.com>
2021-09-16 23:09:46 +0530

    ADDED preselected country's information on the page

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let countries;
+const countriesList = document.getElementById('countries');
 
 fetch("https://restcountries.eu/rest/v2/all")
 .then(res => res.json())
@@ -9,9 +10,33 @@ function initialize(countriesData){
     //console.log(countriesData)
     countries = countriesData;
     let options = "";
+
+    countries.forEach(country => options += `<option value="${country.alpha2Code}">${country.name}</option>"`)
+
+    /*
     for (let i=0; i<countries.length; i++){
-        options += `<option value="${countries[i].alpha2Code}">${countries[i].name}</option>"`;
+        options += `<option value="${countries[i].alpha2Code}">${countries[i].name} (${countries[i].callingCodes[0]})</option>"`;
     }
-    document.getElementById('countries').innerHTML = options;
+    */
+    //document.getElementById('countries').innerHTML = options;
+    //document.querySelector('#countries').innerHTML = options;
+    countriesList.innerHTML = options;
+
+    displayCountryInfo("AF");
+}
+
+function displayCountryInfo(countryByAlpha2Code){
+    const countryData = countries.find(country => country.alpha2Code === countryByAlpha2Code)
+    console.log(countryData); 
+    document.querySelector("#flag-container img").src = countryData.flag;
+    document.querySelector("#flag-container img").alt = `Flag of ${countryData.name}`;
+    document.getElementById("capital").innerHTML = countryData.capital;
+    document.getElementById("dialing-code").innerHTML = countryData.callingCodes[0];
+    document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
+    document.getElementById("currencies").innerHTML = countryData.currencies.map(c => `${c.name} (${c.code})`).join(", ");
+    document.getElementById("region").innerHTML = countryData.region;
+    document.getElementById("subregion").innerHTML = countryData.subregion;
+
+
 }
 
